6a74183572045a4630c69289c279d476bcde8b55
danivanbussel <64401680+danivanbussel@users.noreply.github.com>
2020-04-29 20:25:14 +0200

    i
    i

---
+++ index.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+const bot = new discord.Client();
+
+
+// bot status (NL : Community is online!)
+bot.on("ready", async () => {
+    console.log(`RaceTopia bot is online!`);
+    bot.user.setActivity("test.ip", { type: "PLAYING" });
+});
+
+bot.login(process.env.token);
